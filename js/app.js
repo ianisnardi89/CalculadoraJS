5286d67020cdbec5acc4c563074f0ced8e627936
Isnardi Bieber Ian <ianisnardi89>
2021-11-17 00:21:18 -0300

    First commit

---
+++ js/app.js
@@ -0,0 +1,46 @@
+function sumar(){
+    const forma = document.getElementById('forma');
+    let operandoA = forma['operandoA'];
+    let operandoB = forma['operandoB'];
+    let resultado = parseInt(operandoA.value) + parseInt(operandoB.value);
+    if(isNaN(resultado)){
+        resultado = 'La operacion no cuenta con números'
+    }
+    document.getElementById('resultado').innerHTML = `Resultado: ${resultado}`;
+    console.log(`Resultado: ${resultado}`);
+
+}
+
+function restar(){
+    const forma = document.getElementById('forma');
+    let operandoA = forma['operandoA'];
+    let operandoB = forma['operandoB'];
+    let resultado = parseInt(operandoA.value) - parseInt(operandoB.value);
+    if(isNaN(resultado)){
+        resultado = 'La operacion no cuenta con números'
+    }
+    document.getElementById('resultado').innerHTML = `Resultado: ${resultado}`;
+}
+
+function multiplicar(){
+    const forma = document.getElementById('forma');
+    let operandoA = forma['operandoA'];
+    let operandoB = forma['operandoB'];
+    let resultado = parseInt(operandoA.value) * parseInt(operandoB.value);
+    if(isNaN(resultado)){
+        resultado = 'La operacion no cuenta con números'
+    }
+    document.getElementById('resultado').innerHTML = `Resultado: ${resultado}`;
+
+}
+
+function dividir(){
+    const forma = document.getElementById('forma');
+    let operandoA = forma['operandoA'];
+    let operandoB = forma['operandoB'];
+    let resultado = parseInt(operandoA.value) / parseInt(operandoB.value);
+    if(isNaN(resultado)){
+        resultado = 'La operacion no cuenta con números'
+    }
+    document.getElementById('resultado').innerHTML = `Resultado: ${resultado}`;
+}
